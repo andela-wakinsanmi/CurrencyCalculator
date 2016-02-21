5fe9ab00718d06ded139941a5a3026f3450174b3
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-02-21 13:32:31 +0100

    [#113844195] Creates Currency model class

---
+++ app/src/main/java/com/andela/currencycalculator/model/currency/Currency.java
@@ -0,0 +1,53 @@
+package com.andela.currencycalculator.model.currency;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Spykins on 20/02/2016.
+ */
+public class Currency {
+    private String baseCurrency;
+    private String currency;
+    private double exchangeRate;
+    private String dateCreated;
+
+
+    public Currency(String baseCurrency, double exchangeRate, String currency) {
+        this.baseCurrency = baseCurrency;
+        this.exchangeRate = exchangeRate;
+        this.currency = currency;
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        dateCreated = sdf.format(date);
+    }
+
+    public void setBaseCurrency(String baseCurrency) {
+        this.baseCurrency = baseCurrency;
+    }
+
+    public void setExchangeRate(double exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+
+    public String getBaseCurrency() {
+        return baseCurrency;
+    }
+
+    public double getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+}
