6093a2351c34267e67d4a319aaccd716ec8468e3
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-02-27 20:59:35 +0100

    [#114566513] Implement Currency Manager in Activity main

--- app/src/main/java/com/andela/currencycalculator/MainActivity.java
+++ app/src/main/java/com/andela/currencycalculator/MainActivity.java
@@ -13,77 +13,98 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.andela.currencycalculator.adapter.SpinnerAdapter;
+import com.andela.currencycalculator.calculatorbrain.CalculatorManager;
 import com.andela.currencycalculator.manager.ParserDbManager;
+import com.andela.currencycalculator.model.currency.Currency;
 import com.andela.currencycalculator.model.helper.SpinnerCurrency;
 import com.andela.currencycalculator.model.helper.StringManipulator;
-import com.andela.currencycalculator.model.jsonparser.JsonParserInterface;
+import com.andela.currencycalculator.model.jsonparser.JsonParserListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity implements JsonParserInterface,
+public class MainActivity extends AppCompatActivity implements JsonParserListener,
         AdapterView.OnItemSelectedListener {
 
-    ParserDbManager parserDbManager;
-    Spinner spinnerFrom, spinnerTo;
+    private ParserDbManager parserDbManager;
+    private Spinner spinnerFrom, spinnerTo;
+    private String spinnerFromSelectedtext;
+    private String spinnerToSelectedText;
+    private CalculatorManager calculatorManager;
+    private String inputNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         parserDbManager = ParserDbManager.getInstance(this);
+        parserDbManager.setJsonParserListener(this);
         spinnerTo = (Spinner) findViewById(R.id.spinner_To);
         spinnerFrom = (Spinner) findViewById(R.id.spinner_from);
 
         if(isConnectionAvailable()){
             parserDbManager.getDataFromJson();
         } else if(parserDbManager.hasDatabase()){
-            //load database data
-            notifyActivity();
+            notifyActivity(parserDbManager.readAllCurrencyDataFromDb());
         } else {
             Toast.makeText(this,"You need to be connected to internet", Toast.LENGTH_LONG).show();
         }
+
+        setSpinnerDisplay();
 
 
     }
 
     @Override
     protected void onStart() {
-        //notifyActivity();
         super.onStart();
     }
 
     public void keyPressed(View view) {
         //show in textview...
         TextView inputTextView = (TextView) findViewById(R.id.inputTextView);
-        String oldValueOnView = inputTextView.getText().toString();
-        if (oldValueOnView.equals("0") || oldValueOnView.equals("")) {
+        inputNumber = inputTextView.getText().toString();
+        if (inputNumber.equals("0") || inputNumber.equals("")) {
             String buttonPressed = view.getTag().toString();
             inputTextView.setText(buttonPressed);
+            updateOutputView(buttonPressed);
+
         } else {
             String buttonPressed = view.getTag().toString();
-            inputTextView.setText(oldValueOnView + buttonPressed);
+            inputNumber = inputNumber + buttonPressed;
+            inputTextView.setText(inputNumber);
+            updateOutputView(inputNumber);
 
         }
 
     }
 
+    private void updateOutputView(String buttonPressed){
+        TextView outputView = (TextView) findViewById(R.id.outputText);
+        if (calculatorManager != null){
+            String result = calculatorManager.exchangeFromCurrencyTo(spinnerFromSelectedtext,
+                    spinnerToSelectedText, buttonPressed);
+            outputView.setText(result);
+        }
+
+    }
     public void functionKeyPressed(View view) {
         //send to the calculator brain to calculate
         TextView inputTextView = (TextView) findViewById(R.id.inputTextView);
+        TextView outputView = (TextView) findViewById(R.id.outputText);
         String oldValueOnView = inputTextView.getText().toString();
+
         //send value to calculator brain
         inputTextView.setText("");
+        outputView.setText("");
 
 
     }
 
     public void answerKeyPressed(View view) {
-        spinnerTo.performClick();
+        //get solution from the arraylist in CalculatorManager
 
     }
 
@@ -107,54 +128,68 @@ public class MainActivity extends AppCompatActivity implements JsonParserInterfa
                                int pos, long id) {
         SpinnerCurrency valueSelected = (SpinnerCurrency) parent.getItemAtPosition(pos);
         if (parent.getId() == R.id.spinner_from) {
-            Button buttonFrom = (Button) findViewById(R.id.currency_converting_From);
-            buttonFrom.setText(valueSelected.getSpinnerCodeText());
-            TextView currencyTextInput = (TextView) findViewById(R.id.currencyTextInput);
-            currencyTextInput.setText(parserDbManager.getCurrencyIcon(valueSelected.getSpinnerCodeText()));
-
-            //Do calculation based on what is selected
-
+            updateSpinnerButtonFrom(valueSelected);
         } else {
-            Button buttonFrom = (Button) findViewById(R.id.currency_converting_To);
-            buttonFrom.setText(valueSelected.getSpinnerCodeText());
-            TextView currencyTextAnswer = (TextView) findViewById(R.id.currencyTextAnswer);
-            currencyTextAnswer.setText(parserDbManager.getCurrencyIcon(valueSelected.getSpinnerCodeText()));
-            //Do calculation based on what is selected
+            updateSpinnerButtonTo(valueSelected);
         }
 
     }
 
+    private void updateSpinnerButtonFrom(SpinnerCurrency valueSelected) {
+        Button buttonFrom = (Button) findViewById(R.id.currency_converting_From);
+        spinnerFromSelectedtext = valueSelected.getSpinnerCodeText();
+        buttonFrom.setText(spinnerFromSelectedtext);
+        TextView currencyTextInput = (TextView) findViewById(R.id.currencyTextInput);
+        currencyTextInput.setText(parserDbManager.getCurrencyIcon(spinnerFromSelectedtext));
+        //Do calculation based on what is selected
+        //updateOutputView(inputNumber);
+    }
+
+    private void updateSpinnerButtonTo(SpinnerCurrency valueSelected) {
+        Button buttonTo = (Button) findViewById(R.id.currency_converting_To);
+        spinnerToSelectedText = valueSelected.getSpinnerCodeText();
+        buttonTo.setText(spinnerToSelectedText);
+        TextView currencyTextAnswer = (TextView) findViewById(R.id.currencyTextAnswer);
+        currencyTextAnswer.setText(parserDbManager.getCurrencyIcon(spinnerToSelectedText));
+        //updateOutputView(inputNumber);
+        //Do calculation based on what is selected
+    }
 
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
 
     @Override
-    public void notifyActivity() {
+    public void notifyActivity(ArrayList<Currency> currencies) {
+        //Parsing value from the database/parser into the currency converter....
+        calculatorManager = new CalculatorManager(currencies);
+    }
+
+    private void setSpinnerDisplay(){
         HashMap<String, ArrayList<String>> allCurrenciesInFile = parserDbManager.readAllCurrencyInFile();
-        ArrayList<SpinnerCurrency> currencyKeys = new ArrayList<>();
+            ArrayList<SpinnerCurrency> currencyKeys = new ArrayList<>();
 
-        //Loop through Map and create SpinnerCurrencyOutOfit
+            //Loop through Map and create SpinnerCurrencyOutOfit
 
-        for(Map.Entry<String,ArrayList<String>> entry : allCurrenciesInFile.entrySet()){
-            //the symbol code
-            String code = entry.getKey();
-            if(entry.getValue().size() > 0){
-                String imageName = StringManipulator.replaceSpaceToLower(entry.getValue().get(0));
-                int resID = getResources().getIdentifier(imageName, "drawable", getPackageName());
+            for(Map.Entry<String,ArrayList<String>> entry : allCurrenciesInFile.entrySet()){
+                //the symbol code
+                String code = entry.getKey();
+                if(entry.getValue().size() > 0){
+                    String imageName = StringManipulator.replaceSpaceToLower(entry.getValue().get(0));
+                    int resID = getResources().getIdentifier(imageName, "drawable", getPackageName());
 
-                currencyKeys.add(new SpinnerCurrency(resID, code,imageName));
+                    currencyKeys.add(new SpinnerCurrency(resID, code,imageName));
+                }
+
             }
+            spinnerFrom.setOnItemSelectedListener(this);
+            spinnerTo.setOnItemSelectedListener(this);
 
-        }
-        spinnerFrom.setOnItemSelectedListener(this);
-        spinnerTo.setOnItemSelectedListener(this);
+            SpinnerAdapter adapter = new SpinnerAdapter(this,
+                    R.layout.spinner_layout,R.id.spinnerCodeView, currencyKeys);
 
-        SpinnerAdapter adapter = new SpinnerAdapter(this,
-                R.layout.spinner_layout,R.id.spinnerCodeView, currencyKeys);
-
-        spinnerFrom.setAdapter(adapter);
-        spinnerTo.setAdapter(adapter);
+            spinnerFrom.setAdapter(adapter);
+            spinnerTo.setAdapter(adapter);
 
     }
 
