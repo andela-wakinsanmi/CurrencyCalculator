c66f4fe6d27d5d4df9a43ad8de31f0886382b8c7
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2016-03-04 18:24:59 +0100

    [#114566513] Add new currency in the text file and creates the TopTen file

---
+++ app/src/main/java/com/andela/currencycalculator/model/currency/TopTen.java
@@ -0,0 +1,28 @@
+package com.andela.currencycalculator.model.currency;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Spykins on 04/03/2016.
+ */
+public class  TopTen {
+    private ArrayList<String> topTen;
+
+    private enum MyList{
+        USD,NGN,KWD, BHD,OMR, GBP,EUR,CHF,LYD,BND,SGD,AUD ;
+    }
+
+    public TopTen() {
+        topTen = new ArrayList<>();
+
+        for (MyList myList : MyList.values()){
+            topTen.add(myList.name());
+        }
+    }
+
+    public ArrayList<String> getTopTen(){
+        return topTen;
+    }
+
+
+}
